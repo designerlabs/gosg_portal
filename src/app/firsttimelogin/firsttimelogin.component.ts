0a8e7885f5288ecb0957dda99b84df9f785e0ef7
Sridhar Swaminathan <sridhar.snathan@mimos.my>
2017-12-21 19:33:47 +0800

    updated

--- src/app/firsttimelogin/firsttimelogin.component.ts
+++ src/app/firsttimelogin/firsttimelogin.component.ts
@@ -15,6 +15,12 @@ export class FirsttimeloginComponent implements OnInit {
     
   }
 
+  getRnd(){
+    let url_string = location.href;
+    let url = new URL(url_string);
+    let rnd = url.searchParams.get("rnd");
+    console.log(rnd);
+  }
 
   userIsLogged(){
    return true; 
