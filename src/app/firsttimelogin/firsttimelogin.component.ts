81b72b079d7c65832b0e04e29b4a725accfde408
sridharswaminathan <sridhar.snathan@mimos.my>
2017-12-25 20:41:44 +0800

    updated

--- src/app/firsttimelogin/firsttimelogin.component.ts
+++ src/app/firsttimelogin/firsttimelogin.component.ts
@@ -12,7 +12,7 @@ import {Router, ActivatedRoute, Params} from '@angular/router';
 export class FirsttimeloginComponent implements OnInit {
   getUserData: any;
   userId:string;
-  rndNo:string;
+  rndNo:any;
 
   constructor(
     private protectedservice: ProtectedService,
@@ -27,9 +27,17 @@ export class FirsttimeloginComponent implements OnInit {
   getConfirmation(){
 
 
+    let queryString = window.location.search;
+    let varArray = queryString.split("&");
+    for (let i=0;i<varArray.length;i++) {
+      this.rndNo = varArray[i].split("=");
+        //parameter-value pair
+    }
+    
+
     //this.rndNo = this.activatedRoute.snapshot.queryParams["rnd"];
-    this.rndNo = location.search.split('rnd=')[1]
-    this.protectedservice.completeTran(this.rndNo)
+    //this.rndNo = location.search.split('rnd=')[1]
+    this.protectedservice.completeTran(this.rndNo[1])
         .subscribe(
             userData => {
                 this.getUserData = userData.userTypeList;
