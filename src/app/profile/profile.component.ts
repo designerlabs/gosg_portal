0c5247aa37ede292c69118e9c596492323dde69b
Sridhar Swaminathan <sridhar.snathan@mimos.my>
2018-01-18 11:20:48 +0800

    update

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -274,18 +274,11 @@ export class ProfileComponent implements OnInit, AfterViewInit {
       
                 if(data.user.address.permanentAddressState != null) {
                   if(data.user.address.permanentAddressCountry.countryId == 152) {
-                    // this.profileForm.get('perStateLocal').setValue(data.user.address.permanentAddressState.stateId);
+                    this.profileForm.get('perStateLocal').setValue(data.user.address.permanentAddressState.stateId);
                     this.selectedState = this.profileForm.get('perStateLocal').value;
                     this.getCitiesByStateP(this.selectedState);
-
-                    this.profileForm.get('perStateLocal').setValue(data.user.address.permanentAddressState.stateId);
-                    // this.selectedState = this.profileForm.get('perStateLocal').value;
                     this.profileForm.get('perCityLocal').setValue(data.user.address.permanentAddressCity.cityId); 
                     this.selectedCity = this.profileForm.get('perCityLocal').value;
-
-                    // debugger;
-                    // this.profileForm.get('perCityLocal').setValue(data.user.address.permanentAddressCity.cityId); 
-                    // this.selectedCity = this.profileForm.get('perCityLocal').value;
                   } else {
                     // this.isLocal = false;
                     this.profileForm.get('perStateNotLocal').setValue(data.user.address.permanentAddressState.stateId);
