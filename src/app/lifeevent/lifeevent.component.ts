eca3e9190fff63f55e6a465cf1422c32c4935bd3
Sridhar Swaminathan <sridhar.snathan@mimos.my>
2018-04-26 20:34:36 +0800

    updated

--- src/app/lifeevent/lifeevent.component.ts
+++ src/app/lifeevent/lifeevent.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
 import { Http } from '@angular/http';
 import { APP_CONFIG, AppConfig } from '../config/app.config.module';
+import { SharedService } from '../common/shared.service';
 
 
 @Component({
@@ -12,7 +13,7 @@ import { APP_CONFIG, AppConfig } from '../config/app.config.module';
 export class LifeeventComponent implements OnInit {
   lifeEventData: any[];
   lang = 'en';
-  constructor(private translate: TranslateService, private http: Http, @Inject(APP_CONFIG) private config: AppConfig) {
+  constructor(private translate: TranslateService, private http: Http, @Inject(APP_CONFIG) private config: AppConfig, private sharedService: SharedService) {
     this.lang = translate.currentLang;
     translate.onLangChange.subscribe((event: LangChangeEvent) => {
         const myLang = translate.currentLang;
@@ -31,13 +32,12 @@ export class LifeeventComponent implements OnInit {
     this.getData(this.lang);
   }
 
-  private lifeEventUrl: string = this.config.urlLifeevent;
+  private lifeEventUrl: string = 'life/event';
   getData(lang: string){
-
-         return this.http.get(this.lifeEventUrl + '-' + lang + '.json')
-           .map(res => res.json())
+        
+         return this.sharedService.readPortal(this.lifeEventUrl)
           .subscribe(resSliderData => {
-                this.lifeEventData = resSliderData;
+                this.lifeEventData = resSliderData['contentCategoryResourceList'];
             });
     }
 
