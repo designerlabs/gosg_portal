c6c9583b0809115d7ea7eba3db151eae8f3566ee
redza.rosli <redza.rosli@mimos.my>
2018-06-05 10:30:48 +0800

    added gallery

---
+++ src/app/gallery/gallery.component.ts
@@ -0,0 +1,159 @@
+import { Component, Output, Input, EventEmitter, OnInit, AfterViewChecked, AfterViewInit,  ViewChild, ElementRef, Inject, AfterContentInit } from '@angular/core';
+import { NavService } from '../header/nav/nav.service';
+import { APP_CONFIG, AppConfig } from '../config/app.config.module';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import {TranslateService, LangChangeEvent } from '@ngx-translate/core';
+import { BreadcrumbService } from '../header/breadcrumb/breadcrumb.service';
+import { GalleryService } from './gallery.service';
+
+
+@Component({
+  selector: 'gosg-gallery',
+  templateUrl: './gallery.component.html',
+  styleUrls: ['./gallery.component.css']
+})
+export class GalleryComponent implements OnInit {
+  statusID: any;
+  langIdVal: string;
+  subID: number;
+  moduleName: string;
+
+  @ViewChild('textarea') textarea: ElementRef;
+  @Output() menuClick = new EventEmitter();
+
+  breadcrumb: any;
+  isValid: any;
+  topicID: number;
+  gallery: any[];
+
+  galleryData: any;
+  @Output() langChange = new EventEmitter();
+
+  constructor(
+    public galleryService: GalleryService,  
+    private route: ActivatedRoute, 
+    private navService: NavService, 
+    private translate: TranslateService, 
+    private router: Router, 
+    private breadcrumbService: BreadcrumbService, 
+    @Inject(APP_CONFIG) private config: AppConfig
+  ) {
+    this.lang = translate.currentLang;
+    this.langId = 1;
+
+        translate.onLangChange.subscribe((event: LangChangeEvent) => {
+
+        const myLang = translate.currentLang;
+
+        if (myLang == 'en') {
+
+            translate.get('HOME').subscribe((res: any) => {
+                this.lang = 'en';
+                this.langId = 1;
+                this.moduleName = this.router.url.split('/')[1];
+                this.topicID = parseInt(this.router.url.split('/')[2]);
+                // this.navService.triggerArticle(this.moduleName, this.langId, this.topicID);
+            });
+
+        }
+        if (myLang == 'ms') {
+
+            translate.get('HOME').subscribe((res: any) => {
+                this.lang = 'ms';
+                this.langId = 2;
+                this.moduleName = this.router.url.split('/')[1];
+                this.topicID = parseInt(this.router.url.split('/')[2]);
+                this.subID = parseInt(this.router.url.split('/')[3]);
+                // this.navService.triggerArticle(this.moduleName,  this.langId, this.topicID);
+            });
+        }
+
+
+        // if(this.moduleName == 'subcategory'){
+        //   this.navService.triggerSubArticle(this.subID, this.langId);
+        // }else if(this.moduleName == 'content'){
+        //   this.navService.triggerContent(this.subID, this.langId);
+        // }else{
+        //   this.navService.triggerArticle(this.moduleName,  this.langId, this.topicID);
+        // }
+
+
+
+    });
+
+    if(localStorage.getItem('langID')){
+      this.langIdVal = localStorage.getItem('langID');
+    }else{
+      this.langIdVal = this.langId;
+    }
+
+  }
+
+
+  lang = this.lang;
+  langId = this.langId;
+
+
+
+  ngOnInit() {
+        this.galleryData = this.galleryService.getGallery();
+        console.log(this.galleryData);
+        this.moduleName = this.router.url.split('/')[1];
+        // this.topicID = parseInt(this.router.url.split('/')[2]);
+        // this.navService.triggerArticle(this.moduleName, localStorage.getItem('langID'), this.topicID);
+      }
+
+   getTheme(){
+        return localStorage.getItem('themeColor');
+    }
+
+    clickSideMenu(e, status){
+      this.statusID = status;
+      this.navService.getSubArticleUrl(e.categoryCode, localStorage.getItem('langID'));
+      this.navService.triggerSubArticle(e.categoryCode, localStorage.getItem('langID'));
+      this.router.navigate(['/subcategory', e.categoryCode]);
+      event.preventDefault();
+    }
+
+    clickSideMenuByAgency(e, status){
+      this.navService.getSubArticleUrlByAgency(localStorage.getItem('langID'));
+      this.navService.triggerSubArticleAgency(localStorage.getItem('langID'));
+      this.router.navigate(['/subcategory', 'agency']);
+      event.preventDefault();
+    }
+
+    clickContentFromMenu(pId, aId, status){
+
+      this.statusID = status;
+      this.navService.triggerContent(aId, localStorage.getItem('langID'));
+      this.navService.getContentUrl(aId, localStorage.getItem('langID'));
+      this.router.navigate(['/content', aId]);
+      event.preventDefault();
+    }
+
+
+    // triggerArticle(moduleName, lang, topicID){
+    //     this.route.paramMap
+    //     .switchMap((params: ParamMap) =>
+    //     this.navService.getGalleryData(moduleName, lang, topicID))
+    //     .subscribe(resSliderData => {
+    //         this.gallery = resSliderData;
+    //         this.breadcrumb = this.breadcrumbService.getBreadcrumb();
+    //         this.isValid = this.breadcrumbService.isValid = true;
+    //         this.breadcrumb = this.breadcrumb.name = '';
+    //     });
+    // }
+
+    checkImgData(e){
+        if(e){
+          const chkData = e.search('<img');
+          if (chkData != -1){
+              return true;
+          }else{
+              return false;
+          }
+        }
+
+    }
+
+}
