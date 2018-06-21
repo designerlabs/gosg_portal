09d84cbf609b7a8779423f8aee3b1cb323607c0c
redza.rosli <redza.rosli@mimos.my>
2018-06-21 10:06:47 +0800

    updated search & gallery

--- src/app/gallery/gallery.service.ts
+++ src/app/gallery/gallery.service.ts
@@ -1,24 +1,63 @@
-import { Injectable, Inject, Output, EventEmitter} from '@angular/core';
+import { Injectable, Inject, Output, EventEmitter } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { Subject} from 'rxjs/Subject';
-import { Http, Response} from '@angular/http';
+import { Subject } from 'rxjs/Subject';
+import { Http, Response } from '@angular/http';
 import { APP_CONFIG, AppConfig } from '../config/app.config.module';
-import {ActivatedRoute, Router} from '@angular/router';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
+import { BreadcrumbService } from '../header/breadcrumb/breadcrumb.service';
 
 @Injectable()
 export class GalleryService {
-  @Output() menuClick = new EventEmitter();
-  gallery: any[];
+    @Output() menuClick = new EventEmitter();
+    gallery: any[];
+    breadcrumb: any;
+    isValid: any;
 
-  constructor(private http: Http, @Inject(APP_CONFIG) private config: AppConfig, route: ActivatedRoute, private router: Router) { }
+    constructor(private http: Http, @Inject(APP_CONFIG) private config: AppConfig, private route: ActivatedRoute,private breadcrumbService: BreadcrumbService, private router: Router) { }
 
-  getGallery(){
+    getGallery() {
         return this.gallery;
     }
 
-   public article = {
+    public article = {
         name: 'hello'
     };
 
     private galleryUrl: string = this.config.urlGallery;
+
+    triggerContent(subID, lang) {
+        // alert("Trigger sub acrticle");
+        if (!isNaN(subID)) {
+            this.gallery = [''];
+            return this.route.paramMap
+                .switchMap((params: ParamMap) =>
+                    this.getContentUrl(subID, lang))
+                .subscribe(resSliderData => {
+                    this.gallery = resSliderData;
+                    this.breadcrumb = this.breadcrumbService.getBreadcrumb();
+                    this.isValid = this.breadcrumbService.isValid = true;
+                    this.breadcrumb = this.breadcrumb.name = '';
+                });
+        }
+    }
+
+    getContentUrl(subID: number, lang) {
+        if (!isNaN(subID)) {
+            return this.http.get(this.config.urlPortal + 'content/' + subID + '?language=' + lang)
+                .take(1)
+                .map((response: Response) => response.json().contentCategoryResource.results)
+                // .catch((error:any) =>
+                // Observable.throw(error.json().error || 'Server error')
+                // );
+                .catch(
+                    (err: Response, caught: Observable<any[]>) => {
+                        if (err !== undefined) {
+                            this.router.navigate(['/404']);
+                            return Observable.throw('The Web server (running the Web site) is currently unable to handle the HTTP request due to a temporary overloading or maintenance of the server.');
+                        }
+                        return Observable.throw(caught); // <-----
+                    }
+                );
+        }
+    }
 }
