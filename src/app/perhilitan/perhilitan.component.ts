8972792ed55c4d7785af6c651f6b9b4f675dd02b
Noraini A.Ghani <noraini.ghani@mimos.my>
2018-07-24 19:13:19 +0800

    updated

--- src/app/perhilitan/perhilitan.component.ts
+++ src/app/perhilitan/perhilitan.component.ts
@@ -18,6 +18,12 @@ import { SharedService } from '../common/shared.service';
 import { DialogsService } from '../dialogs/dialogs.service';
 import { TopnavService } from '../header/topnav/topnav.service';
 import {FormBuilder, FormGroup, FormControl, Validators} from '@angular/forms';
+import {
+  Ng4FilesService,
+  Ng4FilesConfig,
+  Ng4FilesStatus,
+  Ng4FilesSelected
+} from '../ng4-files';
 
 @Component({
   selector: 'gosg-perhilitan',
@@ -38,7 +44,6 @@ export class PerhilitanComponent implements OnInit, OnDestroy {
   fourthFormGroup: FormGroup;
   fifthFormGroup: FormGroup;
 
-  pemohonForm: FormGroup;
   namaPemohon: FormControl;
   icPemohon: FormControl;
   phonePemohon: FormControl;
@@ -49,15 +54,46 @@ export class PerhilitanComponent implements OnInit, OnDestroy {
   poskodPemohon: FormControl;
   daerahPemohon: FormControl;
   negeriPemohon: FormControl;
-  
-
-  pemilik1Form: FormGroup;
+    
+  warganegara: FormControl;
+  typeIC: FormControl;
+  icpassport: FormControl;
   namaPemilik: FormControl;
-  
+  phonePemilik: FormControl;
+  jobType: FormControl;
+  jobGroup: FormControl;
+  addPemilik: FormControl;
+  poskodPemilik: FormControl;
+  daerahPemilik: FormControl;
+  negeriPemilik: FormControl;
 
+  mailingAdd: FormControl;
+  mailingPoskod: FormControl;
+  mailingDaerah: FormControl;
+  mailingNegeri: FormControl;
+  copyAdd: FormControl;
 
-  pemilik2Form: FormGroup;
+  companyType: FormControl;
+  jenisMilikan: FormControl;
+  registerType: FormControl;
+  registerNo: FormControl;
+  companyName: FormControl;
+  companyAdd: FormControl;
+  companyPoskod: FormControl;
+  companyDaerah: FormControl;
+  companyNegeri: FormControl;
+  companyPhone: FormControl;
+  companyFax: FormControl;
+  copyAddPemilik: FormControl;
 
+  lsnActivity: FormControl;
+  businessCat: FormControl;
+  file1: FormControl;
+  file2: FormControl;
+  agreement: FormControl;
+
+  public nationality: any;
+  public listjic: any;
   flagPemohon = false;
   flagPemilik = false;
   
@@ -74,6 +110,7 @@ export class PerhilitanComponent implements OnInit, OnDestroy {
     private sharedService: SharedService,
     private _formBuilder: FormBuilder,
     private topnavservice: TopnavService,
+    private ng4FilesService: Ng4FilesService, 
   ) { 
 
     this.lang = translate.currentLang;
@@ -117,34 +154,137 @@ export class PerhilitanComponent implements OnInit, OnDestroy {
       this.langID = 1;
     }
 
-    this.pemohonForm = this._formBuilder.group({
-      namaPemohon: [''],
-      icPemohon: [''],
-      phonePemohon: [''],
-      emailPemohon: [''],
-      add1: [''],
-      add2: [''],
-      add3: [''],
-      poskodPemohon: [''],
-      daerahPemohon: [''],
-      negeriPemohon: ['']
+    // this.firstFormGroup = this._formBuilder.group({
+    //   namaPemohon: [''],
+    //   icPemohon: [''],
+    //   phonePemohon: [''],
+    //   emailPemohon: [''],
+    //   add1: [''],
+    //   add2: [''],
+    //   add3: [''],
+    //   poskodPemohon: [''],
+    //   daerahPemohon: [''],
+    //   negeriPemohon: ['']
+    // });
+
+    this.firstFormGroup = this._formBuilder.group({
+      namaPemohon: new FormControl(),
+      icPemohon: new FormControl(),
+      phonePemohon: new FormControl(),
+      emailPemohon: new FormControl(),
+      add1: new FormControl(),
+      add2: new FormControl(),
+      add3: new FormControl(),
+      poskodPemohon: new FormControl(),
+      daerahPemohon: new FormControl(),
+      negeriPemohon: new FormControl(),
     });
 
-    this.pemilik1Form = this._formBuilder.group({
-      namaPemilik: ['']
+    this.secondFormGroup = this._formBuilder.group({
+      warganegara: new FormControl(),
+      typeIC: new FormControl(),
+      icpassport: new FormControl(),
+      namaPemilik: new FormControl(),
+      phonePemilik: new FormControl(),
+      jobType: new FormControl(),
+      jobGroup: new FormControl(),
+      addPemilik: new FormControl(),
+      poskodPemilik: new FormControl(),
+      daerahPemilik: new FormControl(),
+      negeriPemilik: new FormControl()
     });
-
-    this.firstFormGroup = this.pemohonForm;
-    this.secondFormGroup = this.pemilik1Form;
 
     this.thirdFormGroup = this._formBuilder.group({
-      thirdCtrl: ['', Validators.required]
+      mailingAdd: new FormControl(),
+      mailingPoskod: new FormControl(),
+      mailingDaerah: new FormControl(),
+      mailingNegeri: new FormControl(),
+      copyAdd: new FormControl()
     });
+
     this.fourthFormGroup = this._formBuilder.group({
-      fourthCtrl: ['', Validators.required]
+      companyType: new FormControl(),
+      jenisMilikan: new FormControl(),
+      registerType: new FormControl(),
+      registerNo: new FormControl(),
+      companyName: new FormControl(),
+      companyAdd: new FormControl(),
+      companyPoskod: new FormControl(),
+      companyDaerah: new FormControl(),
+      companyNegeri: new FormControl(),
+      companyPhone: new FormControl(),
+      companyFax: new FormControl(),
+      copyAddPemilik: new FormControl()
     });
+
     this.fifthFormGroup = this._formBuilder.group({
-      fifthCtrl: ['', Validators.required]
+      lsnActivity: new FormControl(),
+      businessCat: new FormControl(),
+      file1: new FormControl(),
+      file2: new FormControl(),
+      agreement: new FormControl()
+    });
+
+    this.getNationality(this.langID);
+    this.getJIC(this.langID);
+  }
+
+  uploadFile1(selectedFiles: Ng4FilesSelected, lan): void {    
+
+    console.log(selectedFiles.files.length);
+
+    let nameFile1 = selectedFiles.files[0].name;
+    console.log(nameFile1);
+
+    this.fifthFormGroup.controls.file1.setValue(nameFile1);
+    // let mFileSize = this.chkUploadFile.maxSize;
+    
+    // let fileExtn = selectedFiles.files[0].name.split('.')[1];
+    // let chkFileExtn = this.resFileExtn.filter(fData => fData === fileExtn.toLowerCase());
+    // if (selectedFiles.status === Ng4FilesStatus.STATUS_SUCCESS) {      
+    //   if (selectedFiles.files.length > 0 && mFileSize) {        
+    //     if (selectedFiles.files[0].size <= mFileSize) {
+    //       // Check File extn  
+    //       if (chkFileExtn.length > 0){
+   
+    //         this.mediaFileUpForm.controls.mediaFileEn.setValue(this.selectedFilesEn);
+    
+    //        }else {
+    //         this.toastr.error('File Extension not match');
+    //        }    
+    //     }else{
+    //       this.toastr.error('File Size Exceed maximum file size');
+    //     }
+    //   }      
+    // }
+  }
+
+  uploadFile2(selectedFiles: Ng4FilesSelected, lan): void {    
+
+    console.log(selectedFiles.files.length);
+
+    let nameFile1 = selectedFiles.files[0].name;
+    console.log(nameFile1);
+
+    this.fifthFormGroup.controls.file2.setValue(nameFile1);
+  }
+
+  getNationality(lang){
+    this.protectedService.getNationalityPerhilitan('perhilitan/dropdown/nationality',lang).subscribe(
+      data => {
+
+        this.nationality = data.perhilitanNationalityResourceList;
+        console.log(this.nationality);
+     
+    });
+  }
+
+  getJIC(lang){
+    this.protectedService.getJenisIC('perhilitan/dropdown/ictype/1',lang).subscribe(
+      data => {
+
+        this.listjic = data.dropdownResourceList;
+     
     });
   }
 
