4ed19ecb8a4d9cbf716d3540e5786bca1e7ed2db
Sridhar Swaminathan <sridhar.snathan@mimos.my>
2018-08-10 12:46:11 +0800

    updated

--- src/app/loader/protected-loader.component.ts
+++ src/app/loader/protected-loader.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'gosg-protected-loader',
-  templateUrl: './protected-loader.component.html',
-  styleUrls: ['./protected-loader.component.css']
+  templateUrl: './portal-loader.component.html',
+  styleUrls: ['./portal-loader.component.css']
 })
 export class ProtectedLoaderComponent implements OnInit {
 
--- src/app/loader/shared-loader.component.ts
+++ src/app/loader/shared-loader.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'gosg-shared-loader',
-  templateUrl: './shared-loader.component.html',
-  styleUrls: ['./shared-loader.component.css']
+  templateUrl: './portal-loader.component.html',
+  styleUrls: ['./portal-loader.component.css']
 })
 export class SharedLoaderComponent implements OnInit {
 
