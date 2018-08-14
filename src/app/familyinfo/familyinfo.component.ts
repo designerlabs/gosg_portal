4f8eda08dafafe46705f6e2ae2ec64fd760f740e
Noraini A.Ghani <noraini.ghani@mimos.my>
2018-08-14 16:22:10 +0800

    updated family info

---
+++ src/app/familyinfo/familyinfo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gosg-familyinfo',
+  templateUrl: './familyinfo.component.html',
+  styleUrls: ['./familyinfo.component.css']
+})
+export class FamilyinfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
