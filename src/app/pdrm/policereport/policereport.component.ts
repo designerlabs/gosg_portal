b48d69d6d91a25d377323b0e510dbb67f7f74793
Noraini A.Ghani <noraini.ghani@mimos.my>
2018-10-05 10:56:38 +0800

    updated

--- src/app/pdrm/policereport/policereport.component.ts
+++ src/app/pdrm/policereport/policereport.component.ts
@@ -208,7 +208,8 @@ export class PolicereportComponent implements OnInit, OnDestroy {
     //   this.loading = false;
     // });
 
-    this.dataAnnouncement = '<table> <tr> <td align="left"> <b>Perhatian: <br /> <i>Semua jenis Repot Polis bertarikh dari 1 Ogos 2011 hingga kini (kecuali Repot Kemalangan Jalan Raya) yang dibuat  di kawasan Kontinjen Kuala Lumpur dan Selangor boleh disemak buat masa ini. </i></b> </td> </tr> </table>'
+    this.dataAnnouncement = '<table> <tr> <td align="left"> <b>Perhatian: <br /> <i>Semua jenis Repot Polis bertarikh dari 1 Ogos 2011 hingga kini (kecuali Repot Kemalangan Jalan Raya) yang dibuat  di kawasan Kontinjen Kuala Lumpur dan Selangor boleh disemak buat masa ini. </i></b> </td> </tr> </table>';
+    console.log(this.dataAnnouncement);
   }
 
   convert(data){
